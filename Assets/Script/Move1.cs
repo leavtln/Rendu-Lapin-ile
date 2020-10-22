9d770420e384c8b511567ccf14d53fe3c5b084d8
leavtln <leavtln@users.noreply.github.com>
2020-10-22 16:33:50 +0200

    premier commit

---
+++ Assets/Script/Move1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move1 : MonoBehaviour
+
+{
+	void Update()
+
+	{
+
+		Vector3 move = new Vector3();
+
+		if (Input.GetKey(KeyCode.UpArrow))
+
+			move.z += 0.1f;
+
+		if (Input.GetKey(KeyCode.DownArrow))
+
+			move.z -= 0.1f;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+
+			move.x -= 0.1f;
+
+		if (Input.GetKey(KeyCode.RightArrow))
+
+			move.x += 0.1f;
+
+		transform.position += move;
+
+	}
+}
