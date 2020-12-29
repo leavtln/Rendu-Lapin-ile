114b1b0b987ff2478bf50f07e515ecb833c6995f
leavtln <vitalonelea@gmail.com>
2020-12-29 21:54:48 +0100

    Add_Scripts
    rigidbody perso
    script camera

---
+++ Assets/Camera_Follow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 position_set;
+
+    void LateUpdate()
+    {
+        transform.position = target.position + position_set;
+    }
+}
